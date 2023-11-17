c9db060a70786c84f953c132255970a5a0559701
jsneath <jamessneath1@gmail.com>
2023-11-17 20:07:32 +0000

    when user searches - get city data from weather API and show as object in console

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-var test = document.getElementsByClassName("weather-header")
 
-$(test).css("color", "green")
 
-var url = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+$("button").on("click", function (event) {
+   event.preventDefault()
+   var searchItem = document.querySelector(".weather-search")
+    var cityName = searchItem.value
+    var key = "94f82e011baf95e0162c81c9aa8a34fe"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key
+ 
+  
+    // Performing an Fetch request with the queryURL
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      // After data comes back from the request
+      .then(function (data) {
+        console.log(queryURL);
+  
+        console.log(data);
+        // storing the data from the Fetch request in the results variable
+        var results = data.data;
+  
+        // }
+      });
+  });
