03b57a02a34ef01b074c2edf8a4a478039d8102a
jsneath <jamessneath1@gmail.com>
2023-11-20 22:15:11 +0000

    functional cards showing weather current and forecast

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var todaysWeather = document.querySelector("#cityName")
+var today = dayjs().format('DD/MM/YYYY');
+
+
+
+
 
 
 $("button").on("click", function (event) {
@@ -6,8 +12,36 @@ $("button").on("click", function (event) {
     var cityName = searchItem.value
     var key = "94f82e011baf95e0162c81c9aa8a34fe"
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key
+
+    var todayQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key
  
   
+    fetch(todayQueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    // After data comes back from the request
+    .then(function (data) {
+      console.log(todayQueryURL);
+
+      console.log(data);
+      // storing the data from the Fetch request in the results variable
+      
+      todaysWeather.textContent= cityName
+      
+         var temp = data.main.temp
+         var wind = data.wind.speed
+         var humidity = data.main.humidity
+         var card = document.getElementById('card-1')
+
+         card.querySelector('.card-date').textContent = "Date: " + today;
+         card.querySelector('.card-temp').textContent = "Temperature: " + temp + "°C";
+         card.querySelector('.card-wind').textContent = "Wind Speed: " + wind + " km/h";
+         card.querySelector('.card-humid').textContent = "Humidity: " + humidity + "%";
+              
+    });
+
+
     // Performing an Fetch request with the queryURL
     fetch(queryURL)
       .then(function (response) {
@@ -19,17 +53,29 @@ $("button").on("click", function (event) {
   
         console.log(data);
         // storing the data from the Fetch request in the results variable
-        var results = JSON.stringify(data.list[0], null, 2)
-        var temp = data.list[0].main.temp
-        var wind = data.list[0].wind.speed
-        var humidity = data.list[0].main.humidity
-        var date = data.list[0].dt_txt
-        console.log(temp)
-        console.log(wind)
-        console.log(humidity)
-        console.log(date)
 
-        console.log("the results are" + results)
-        // }
+        for (i = 0, cardNumber = 2; i <= data.list.length; i +=8, cardNumber++) {
+            
+            if (data.list[i]) {
+                var temp = data.list[i].main.temp
+                var wind = data.list[i].wind.speed
+                var humidity = data.list[i].main.humidity
+                var date = data.list[i].dt_txt
+
+                var card = document.getElementById('card-' + cardNumber);
+
+                card.querySelector('.card-date').textContent = "Date: " + date;
+                card.querySelector('.card-temp').textContent = "Temperature: " + temp + "°C";
+                card.querySelector('.card-wind').textContent = "Wind Speed: " + wind + " km/h";
+                card.querySelector('.card-humid').textContent = "Humidity: " + humidity + "%";
+                
+
+                console.log(temp)
+                console.log(wind)
+                console.log(humidity)
+                console.log(date)
+            } 
+        }
+        
       });
   });
